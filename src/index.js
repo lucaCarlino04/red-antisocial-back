a1c61aa1b5d61d34939aae12179659f0d32a1616
lucaCarlino04 <lucacarlinounahur@gmail.com>
2026-06-10 19:23:34 -0300

    Configuraciones principales

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("UnaHur - Anti-Social net");
+const mongoose = require("mongoose");
+
+async function conectarDB() {
+  try {
+    await mongoose.connect(
+      "mongodb://admin:admin123@localhost:27017/redantisocial?authSource=admin"
+    );
+
+    console.log("MongoDB conectado");
+  } catch (error) {
+    console.error("Error al conectar MongoDB:", error);
+  }
+}
+
+conectarDB();
