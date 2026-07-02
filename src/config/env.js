95aa2007ce0de2fd4e254fd09d14bd1b1a32266b
lucaCarlino04 <lucacarlinounahur@gmail.com>
2026-07-02 02:23:55 -0300

    Volvi a hardcodear el port pero igual se configura por variable de entorno.

--- src/config/env.js
+++ src/config/env.js
@@ -2,9 +2,11 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
 const config = {
-  port: parseInt(process.env.PORT) || 3000,
+  port: 7860 || parseInt(process.env.PORT) || 3000,
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/redantisocial",
   commentVisibilityMonths: parseInt(process.env.COMMENT_VISIBILITY_MONTHS) || 6
 };
+
+REDIS_URL="rediss://default:gQAAAAAAARhEAAIgcDJiNTk3Zjg5YWNjY2M0N2ZlYWM0MDMxZGU3NDRjZWQ0ZA@rich-deer-71748.upstash.io:6379"
 
 module.exports = config;
