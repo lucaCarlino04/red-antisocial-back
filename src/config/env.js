da2da2f908ddbcdd4cef121bfbb611fb58384f98
lucaCarlino04 <lucacarlinounahur@gmail.com>
2026-07-02 02:06:51 -0300

    hardcodeado temporalmente el port

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
 const config = {
-  port: parseInt(process.env.PORT) || 7860,
+  port: 7860,
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/redantisocial",
   commentVisibilityMonths: parseInt(process.env.COMMENT_VISIBILITY_MONTHS) || 6
 };
